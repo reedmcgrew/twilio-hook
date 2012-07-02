f45b97815311fdb6bd2b7b1df8ac11b5cdc5ee3a
Reed McGrew <reedmcgrew1185@gmail.com>
2012-07-02 14:18:45 -0600

    Added settings example

--- settings.js
+++
@@ -1,8 +0,0 @@
-module.exports = { 
-  "twilio": {
-    "sID"       : "AC3ce5cf5c736de2c0b535a65eb5b55628",
-    "authToken" : "477eef970c1bd4cc062d5a39369f06dc",
-    "sendUrl"   : 'https://api.twilio.com/2010-04-01/Accounts/AC3ce5cf5c736de2c0b535a65eb5b55628/SMS/Messages.json',
-    "from"      : '+14155992671'
-  }
-}
